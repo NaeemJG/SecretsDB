050f72ad7b9a14a1141d611b4e1e9efd48b60688
Jamil Naeem <jamil.naeem17@gmail.com>
2020-05-13 10:22:03 -0400

    Initialized PassportJS

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const express = require('express')
 const app = express();
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
+const session  = require('express-session')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.set('view engine', 'ejs')
+app.use(session({
+    secret: 'whatthisgottacontainasecret',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 mongoose.connect('mongodb://localhost:27017/secretsDB', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model('User', userSchema)
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.listen(3000, () => {
     console.log(`We learning about Auth and Sec`)
 })
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -31,35 +45,13 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 app.post('/register', (req, res) => {
-    const user = new User({
-        email: req.body.username,
-        password: req.body.password
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        
     })
-    user.save((err) => {
-        if (!err) {
-            res.render('secrets')
-        } else {
-            res.send(`Something went wrong...${err}`)
-        }
-    });
-    // res.redirect('/login')
 })
 
 app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email: username}, (err, result) => {
-        if(!err) {
-            if(result) {
-                if (result.password === password) {
-                    res.render('secrets')
-                }
-            }
-        } else {
-            res.send(`Something went wrong...${err}`)
-        }
-    })
 })
 
 app.get('/logout', (req, res) => {
